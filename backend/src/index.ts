4bdf30eba19031085f63014c1831e5055012e85c
hahoangtuanhung <hung.hht204751@sis.hust.edu.vn>
2024-10-31 00:48:55 +0700

    TH - change account database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,16 +3,18 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=> console.log("Connected to database!"));
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get("/test", async (req: Request, res: Response) => {
-    res.json({ message: "Hello VN" });
+  res.json({ message: "Hello VN" });
 });
 
 app.listen(5000, () => {
-    console.log("Server is starting on port 5000");
+  console.log("Server is starting on port 5000");
 });
