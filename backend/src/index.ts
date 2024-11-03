a16606839fca9a688aa2f095db6b5543eef1c5a6
hungbkk66 <laiviethung2003@gmail.com>
2024-11-03 21:02:28 +0700

    create restaurant api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,41 +1,34 @@
-<<<<<<< HEAD
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=> console.log("Connected to database!"));
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.use("/api/restaurant", restaurantRoute);
-app.get("/test", async (req: Request, res: Response) => {
-    res.json({ message: "Hello VN" });
-});
-
-app.listen(5000, () => {
-    console.log("Server is starting on port 5000");
-});
-=======
-import express, { Request, Response } from "express";
-import cors from "cors";
-import "dotenv/config";
-import mongoose from "mongoose";
-import restaurantRoute from "./routes/RestaurantRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to database!"));
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
-app.use(express.json());
+
 app.use(cors());
 
+app.use(express.json());
 
+
+
+
+app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
+
 app.get("/test", async (req: Request, res: Response) => {
   res.json({ message: "Hello VN" });
 });
@@ -43,4 +36,3 @@ app.get("/test", async (req: Request, res: Response) => {
 app.listen(5000, () => {
   console.log("Server is starting on port 5000");
 });
->>>>>>> 1fbded8031a17bbc58cc47f3830baff3b1515a9c
