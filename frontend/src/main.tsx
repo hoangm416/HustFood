66ec11899788122bbf03e94452213d153beaed52
hahoangtuanhung <hung.hht204751@sis.hust.edu.vn>
2024-11-03 18:06:21 +0700

    TH-Auth0 setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRouters";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 // import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      {/* <Auth0ProviderWithNavigate> */}
+      <Auth0ProviderWithNavigate>
         <AppRoutes />
-      {/* </Auth0ProviderWithNavigate> */}
+      </Auth0ProviderWithNavigate>
     </Router>
   </React.StrictMode>
 );
