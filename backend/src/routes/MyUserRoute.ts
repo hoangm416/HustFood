2989f1c1bb84683ac5bf0312b8f805135abae2a8
hahoangtuanhung <hung.hht204751@sis.hust.edu.vn>
2024-11-17 17:23:58 +0700

    TH - Validate Access Token

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
 // /api/my/user
-router.post("/", MyUserController.createCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser); // K hiểu sao lỗi phần này
 
 export default router;
