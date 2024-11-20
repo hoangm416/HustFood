49222310f43629b8e6587b1741b7b4158a67d604
hahoangtuanhung <hung.hht204751@sis.hust.edu.vn>
2024-11-21 01:52:47 +0700

    TH - Pre-populate user profile form

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,19 +1,25 @@
-
 import { useGetMyUser, useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/manage-restaurant-form/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
-    const { currentUser, isLoading: isGetLoading } = useGetMyUser();
-    const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
+  const { currentUser, isLoading: isGetLoading } = useGetMyUser();
+  const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
 
-    if(isGetLoading) {
-      return <span>Loading ...</span>;
-    }
+  if (isGetLoading) {
+    return <span>Loading ...</span>;
+  }
 
-  return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading} />;
+  if (!currentUser) {
+    return <span>Unable to load user profile</span>;
+  }
 
-
-
+  return (
+    <UserProfileForm
+      currentUser={currentUser}
+      onSave={updateUser}
+      isLoading={isUpdateLoading}
+    />
+  );
 };
 
 export default UserProfilePage;
