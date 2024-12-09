ffa070faa83369d94939bdc70871af5049731018
hungbkk66 <laiviethung2003>
2024-12-10 04:48:15 +0700

    fix create restaurant

--- backend/src/controllers/MyRestaurantController.ts
+++ backend/src/controllers/MyRestaurantController.ts
@@ -3,6 +3,20 @@ import Restaurant from "../models/restaurant";
 import cloudinary from "cloudinary";
 import mongoose from "mongoose";
 
+const getMyRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.userId });
+    if (!restaurant) {
+      res.status(404).json({ message: "không tìm thấy nhà hàng" });
+      return;
+    }
+    res.json(restaurant);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Lỗi khi tìm nạp nhà hàng" });
+  }
+};
+
 const createMyRestaurant = async (req: Request, res: Response) => {
     try {
       const existingRestaurant = await Restaurant.findOne({ user: req.userId });
@@ -12,17 +26,13 @@ const createMyRestaurant = async (req: Request, res: Response) => {
         return;
       }
   
-      const image = req.file as Express.Multer.File;
-      const base64Image = Buffer.from(image.buffer).toString("base64");
-      const dataURI = `data:${image.mimetype};base64, ${base64Image}`;
+      const imageUrl = await uploadImage(req.file as Express.Multer.File);
     
-      const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
-
       const restaurant = new Restaurant(req.body);
-      restaurant.imageUrl = uploadResponse.url;
-      restaurant.user = new mongoose.Types.ObjectId(req.userId);
-      restaurant.lastUpdated = new Date();
-      await restaurant.save();
+    restaurant.imageUrl = imageUrl;
+    restaurant.user = new mongoose.Types.ObjectId(req.userId);
+    restaurant.lastUpdated = new Date();
+    await restaurant.save();
 
       res.status(201).send(restaurant);
     } catch (error) {
@@ -31,19 +41,7 @@ const createMyRestaurant = async (req: Request, res: Response) => {
     }
   };
 
-  const getMyRestaurant = async (req: Request, res: Response) => {
-    try {
-      const restaurant = await Restaurant.findOne({ user: req.userId });
-      if (!restaurant) {
-       res.status(404).json({ message: "không tìm thấy nhà hàng" });
-       return;
-      }
-      res.json(restaurant);
-    } catch (error) {
-      console.log("error", error);
-      res.status(500).json({ message: "Lỗi khi tìm nạp nhà hàng" });
-    }
-  };
+
 
   const updateMyRestaurant = async (req: Request, res: Response) => {
     try {
